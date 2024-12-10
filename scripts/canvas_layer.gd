6017b2af12be8cc370f6320c01a09f7fc6daa580
Koskali <132392361+Koskali@users.noreply.github.com>
2024-12-10 02:16:41 +0100

    update "esc" pause

--- scripts/canvas_layer.gd
+++ scripts/canvas_layer.gd
@@ -2,9 +2,22 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	set_visible(false)
-
+	
+func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_cancel"): 
 		set_visible(true)
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		get_tree().paused = true
+
+
+
+func _on_resume_pressed() -> void:
+	get_tree().paused = false
+	set_visible(false)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
