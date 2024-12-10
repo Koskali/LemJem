17753ca12e013f256d8c235229d79eeba1b8e2fe
Koskali <132392361+Koskali@users.noreply.github.com>
2024-12-10 14:49:59 +0100

    Hud_with_mask_piece update

--- scenes/rich_text_label.gd
+++ scenes/rich_text_label.gd
@@ -1,7 +1,7 @@
 extends RichTextLabel
 
 var mask_count = 0
-var count
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,11 +11,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	OS.delay_msec(100)
+	
 	self.text =""
 	var mask_icon = Texture.new()
 	mask_icon = load("res://assets/2d/próbny_kawałek_maski.jpeg")
 	add_image(mask_icon,40,40)
-	count = (" : " + str(mask_count))
+	var count = (" : " + str(mask_count))
 	add_text(count)
 	mask_count += 1
