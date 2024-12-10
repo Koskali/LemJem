b14a57c6448d8280550e408e30fc7a237a5769b5
Jakub <w00jcikjakub@gmail.com>
2024-12-10 03:55:51 +0100

    enemy

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,10 +1,9 @@
 extends CharacterBody3D
 
-
-
-
-
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player_fp"):
 		print("enemy 1 approached")
-		$Area3D.queue_free()
+		get_tree().change_scene_to_file("res://scenes/grid/battlescene.tscn")
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	$Area3D.queue_free()
