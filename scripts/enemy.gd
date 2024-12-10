00a8c491a9b3d2728c8b937737c04c9d8b409f0d
rzabka1 <jturovska@gmail.com>
2024-12-10 01:50:22 +0100

    Enemy script fixed

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,12 +1,10 @@
 extends CharacterBody3D
 
 
-<<<<<<< Updated upstream
-=======
+
 func _ready() -> void:
 	print("dupa")
 
->>>>>>> Stashed changes
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player_fp"):
