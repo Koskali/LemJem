c25d678e707a55346be40f3d23b31a357c834cfc
rzabka1 <jturovska@gmail.com>
2024-12-10 03:26:30 +0100

    More enemies with interactions

--- scripts/enemy_fp_2.gd
+++ scripts/enemy_fp_2.gd
@@ -2,8 +2,6 @@ extends CharacterBody3D
 
 
 
-
-
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player_fp"):
-		print("enemy 1 approached")
+		print("enemy 2 approached")
