1155fc84134e50b8142b9d402e6f63bc1977fca6
Jakub <w00jcikjakub@gmail.com>
2024-12-15 11:09:06 +0100

    Enemy movement improved
    Changed the way enemy moves, it mirrors the player's now.

--- scenes/grid/enemy_battle.gd
+++ scenes/grid/enemy_battle.gd
@@ -1,10 +1,23 @@
 extends Area2D
 
 var enemy_pos = [320, 448, 576, 704, 832]
+var current_index = 2
 
+# Rozwinąłem tą funkcję o linijki 9 do 19
 func change_enemy_position():
 	if Global.enemy_dead == false:
-		position.x = enemy_pos.pick_random()
+		var possible_moves = []
+		
+		# Ruch w lewo, jeśli może
+		if current_index > 0:
+			possible_moves.append(current_index - 1)
+		
+		# Ruch w prawo, jeśli może
+		if current_index < enemy_pos.size() - 1:
+			possible_moves.append(current_index + 1)
+		
+		current_index = possible_moves.pick_random()
+		position.x = enemy_pos[current_index]
 
 func spawn_enemy_projectile():
 	print("enemy")
